dc157dd360dccc8657f604c993149472b5210e38
xinzhangshun <1073428413@qq.com>
2019-03-16 11:46:54 +0800

    init code

---
+++ src/main/java/com/genericFrame/controller/GenericController.java
@@ -0,0 +1,12 @@
+package com.genericFrame.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GenericController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello world";
+    }
+}
